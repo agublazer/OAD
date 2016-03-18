75c5d709922f666ddc67ec78d5d1bc5b9f38ac27
FreAlv <FreAlv@users.noreply.github.com>
2016-03-17 21:41:39 -0500

    suma, resta, multiplicación con modulos

---
+++ Tarea1/modulo.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+int modSuma(int numA, int numB, int mod){
+    int res = numA%mod + numB%mod;
+    return res;
+}
+
+int modResta(int numA, int numB,int mod){
+    int res = numA - numB;
+    if (res < 0){ //Si es negativo se convirte en positivo
+        res*=-1;
+    }
+    res%=mod;
+    return res;
+}
+
+int modMult(int numA, int numB,int mod){
+    int res = (numA*numB)%mod;
+    return res;
+}
+
+int opciones(){
+    std::cout << "Elija una opcion:\n";
+}
+
+int main(){
+    int opcion;
+    int numA;
+    int numB;
+    int mod;
+
+    std::cout << "Elija una opcion:\n";
+    std::cout << "1. Sumar\n";
+    std::cout << "2. Restar\n";
+    std::cout << "3. Multiplicar\n";
+    std::cin >> opcion;
+    std::cout << "Ingrese el primer numero: ";
+    std::cin >> numA;
+    std::cout << "Ingrese el segundo numero: ";
+    std::cin >> numB;
+    std::cout << "Ingrese el modulo: ";
+    std::cin >> mod;
+
+    if (opcion == 1){
+        std::cout << modSuma(numA, numB, mod) << std::endl;
+    }
+    if(opcion == 2){
+        std::cout << modResta(numA, numB, mod) << std::endl;
+    }
+    if(opcion == 3){
+        std::cout << modMult(numA, numB, mod) << std::endl;
+    }
+    return 0;
+}
