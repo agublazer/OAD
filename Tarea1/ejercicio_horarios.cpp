ff6fa10410fa71c0bc143bbb12e911bde3e09d50
agublazer <dany.diaz@ucsp.edu.pe>
2016-03-19 18:03:50 -0500

    Ejercicio de horarios
    ggwp

---
+++ Tarea1/ejercicio_horarios.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+float convertir_minutos(float hora)
+{
+    int hora_1 = hora;
+    float minutos = hora_1*60 + (hora - hora_1)*100;
+    return hora_1*60 + (hora - hora_1)*100;
+}
+
+void convertir_horas(int minutos,bool formato)
+{
+    float hora;
+    float minutos_1 = (minutos%60);
+    hora = minutos/60;
+    if (formato==true)
+    {
+        if (minutos_1<10 and hora<10)
+            cout<<"0"<<hora<<":"<<"0"<<minutos_1;
+        if (minutos_1>=10 and hora<10)
+            cout<<"0"<<hora<<":"<<minutos_1;
+        if (minutos_1<10 and hora>=10)
+            cout<<hora<<":"<<"0"<<minutos_1;
+        if (minutos_1>=10 and hora>=10)
+            cout<<hora<<":"<<minutos_1;
+    }
+    if (formato==false)
+    {
+        if ((minutos/60)>12 and minutos_1<10)
+            cout<<(minutos/60)-12<<":"<<"0"<<minutos_1<<" pm";
+        if ((minutos/60)>12 and minutos_1>=10)
+            cout<<(minutos/60)-12<<":"<<minutos_1<<" pm";
+        if ((minutos/60)<=12 and minutos_1<10)
+            cout<<(minutos/60)<<":"<<"0"<<minutos_1<<" am";
+        if ((minutos/60)<=12 and minutos_1>=10)
+            cout<<(minutos/60)<<":"<<minutos_1<<" am";
+
+    }
+
+}
+
+void imprimir_horario(float minutos_entrada, float minutos_salida, float duracion_hora, bool formato_hora)
+{
+    float minutos_total = minutos_salida - minutos_entrada;
+    float total_horas = (float)(minutos_salida - minutos_entrada)/(float)(duracion_hora);
+    float hora_1,hora_2;
+
+    if (minutos_total<=0 or minutos_total<duracion_hora)
+        cout<<"Horario no valido"<<endl;
+    else
+    {
+        hora_1=minutos_entrada;
+        for(int i=0;i<total_horas;i++)
+        {
+            hora_2= hora_1+duracion_hora;
+            if (hora_2<=minutos_salida)
+            {
+                convertir_horas(hora_1,formato_hora);
+                cout<<" - ";
+                convertir_horas(hora_2,formato_hora);
+                cout<<endl;
+            }
+            hora_1=hora_2;
+        }
+    }
+}
+int main()
+{
+    //variables de entrada:
+    float hora_inicio;
+    float hora_fin;
+    float duracion_hora;
+    bool formato_hora;
+
+    cout<<"Datos: "<<endl;
+    cout<<"Inicio: ";cin>>hora_inicio;
+    cout<<"Fin: ";cin>>hora_fin;
+    cout<<"Duracion: ";cin>>duracion_hora;
+    cout<<"Formato hora: (0=am/pm,1=normal)";cin>>formato_hora;
+
+    float minutos_inicio = convertir_minutos(hora_inicio);
+    float minutos_fin = convertir_minutos(hora_fin);
+
+    imprimir_horario(minutos_inicio,minutos_fin,duracion_hora,formato_hora);
+
+
+
+
+}
