5eb56e99a0bdb9ab7e08e6211b44920dea621f89
Valentino <dany.diaz@ucsp.edu.pe>
2016-04-03 18:15:07 -0500

    Create quicksort.cpp

---
+++ Tarea2/quicksort.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+const int t= 400;
+
+int *lista;
+
+void crearLista()
+{
+    lista = new int [t];
+    for(int i=0;i<t;i++)
+        lista[i]= rand()%100+1;
+
+}
+
+int divide(int *array, int inicio, int fin) {
+    int izquierda;
+    int derecha;
+    int pivot;
+    int temp;
+
+    pivot = array[inicio];
+    izquierda = inicio;
+    derecha = fin;
+
+    while (izquierda < derecha) {
+        while (array[derecha] > pivot) {
+            derecha--;
+        }
+
+        while ((izquierda < derecha) and (array[izquierda] <= pivot)) {
+            izquierda++;
+        }
+
+        if (izquierda < derecha) {
+            temp = array[izquierda];
+            array[izquierda] = array[derecha];
+            array[derecha] = temp;
+        }
+    }
+
+    temp = array[derecha];
+    array[derecha] = array[inicio];
+    array[inicio] = temp;
+    return derecha;
+
+}
+
+void quicksort(int *array, int inicio, int fin)
+{
+    int pivot;
+    if (inicio < fin) {
+        pivot = divide(array, inicio, fin);
+        quicksort(array, inicio, pivot - 1);
+        quicksort(array, pivot + 1, fin);
+    }
+}
+int main()
+{
+    crearLista();
+    quicksort(lista,0, t-1);
+    for(int i=0;i<t;i++)
+        cout<<lista[i]<<" ";
+    return 0;
+}
